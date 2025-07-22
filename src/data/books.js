483ab06a6b85befcc8fb0a0fc91d22441e9f6050
Gigi <dergigi@pm.me>
2025-07-22 21:22:06 +0200

    feat: add /books page with initial book list

---
+++ src/data/books.js
@@ -0,0 +1,58 @@
+const books = [
+  {
+    title: "The Sovereign Individual",
+    author: "James Dale Davidson & William Rees-Mogg",
+    description: "Mastering the Transition to the Information Age.",
+    cover: "/images/books/the-sovereign-individual.jpg",
+    links: [
+      {
+        name: "Amazon",
+        url: "https://www.amazon.com/Sovereign-Individual-Mastering-Transition-Information/dp/0684832720",
+      },
+      {
+        name: "Audible",
+        url: "https://www.audible.com/pd/The-Sovereign-Individual-Audiobook/B07X3Z4T5G",
+      },
+    ],
+  },
+  {
+    title: "The Cathedral and the Bazaar",
+    author: "Eric S. Raymond",
+    description:
+      "Musings on Linux and Open Source by an Accidental Revolutionary.",
+    cover: "/images/books/the-cathedral-and-the-bazaar.jpg",
+    links: [
+      {
+        name: "Amazon",
+        url: "https://www.amazon.com/Cathedral-Bazaar-Musings-Accidental-Revolutionary/dp/0596001088",
+      },
+      {
+        name: "Read for free",
+        url: "http://www.catb.org/~esr/writings/cathedral-bazaar/",
+      },
+    ],
+  },
+  {
+    title: "Free Software, Free Society",
+    author: "Richard Stallman",
+    description:
+      "Selected Essays of Richard M. Stallman. When we speak of free software, we are referring to freedom, not price.",
+    cover: "/images/books/free-software-free-society.jpg",
+    links: [
+      {
+        name: "Official Website",
+        url: "https://www.gnu.org/philosophy/fsfs/fsfs.html",
+      },
+      {
+        name: "Amazon",
+        url: "https://www.amazon.com/Free-Software-Society-Selected-Stallman/dp/1441499533",
+      },
+      {
+        name: "Read for free",
+        url: "https://www.gnu.org/philosophy/fsfs/rms-essays.pdf",
+      },
+    ],
+  },
+];
+
+export default books; 
