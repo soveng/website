28f08de15f5276d6bac1b14c9d4d74286bde2506
Gigi <dergigi@pm.me>
2025-11-30 23:06:00 +0100

    chore: inline fathom site id

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,15 +1,1 @@
 /// <reference types="astro/client" />
-
-interface ImportMetaEnv {
-  /**
-   * Fathom site ID, used by astro-fathom.
-   * Example: "ABCDEFGH"
-   */
-  readonly FATHOM_SITE?: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
-
-
